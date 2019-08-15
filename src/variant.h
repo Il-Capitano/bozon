f15a69b92127e54f3f901859285e6c0940be1f13
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2019-08-15 18:02:43 +0200

    Update variant.h

--- src/variant.h
+++ src/variant.h
@@ -57,6 +57,26 @@ struct _get_id
 	static constexpr int value = _get_id_func<T, First, Ts...>();
 };
 
+template<typename T, typename First, typename ...Ts>
+constexpr int _get_id_v = _get_id<T, First, Ts...>::value;
+
+
+template<int first, int ...vals>
+struct _max_of
+{
+	static constexpr int value = first > _max_of<vals...>::value ? first : _max_of<vals...>::value;
+};
+
+template<int first>
+struct _max_of<first>
+{
+	static constexpr int value = first;
+};
+
+template<int first, int ...vals>
+constexpr int _max_of_v = _max_of<first, vals...>::value;
+
+
 
 template<typename ...Ts>
 class variant
@@ -66,70 +86,102 @@ public:
 	using self_t = variant<Ts...>;
 
 private:
-	void *_data;
+	alignas(_max_of_v<alignof(Ts)...>) char _data[_max_of_v<sizeof(Ts)...>];
 	int _type_id;
 
-	variant(void *data, int type_id)
-		: _data(data), _type_id(type_id)
-	{}
+
+	template<typename T>
+	T &no_check_get(void)
+	{
+		return *reinterpret_cast<T *>(this->_data);
+	}
+
+	template<typename T>
+	T const &no_check_get(void) const
+	{
+		return *reinterpret_cast<const T *>(this->_data);
+	}
+
+	template<typename T, typename ...Args>
+	void no_check_emplace(Args &&...args)
+	{
+		new(this->_data) T(std::forward<Args>(args)...);
+	}
+
+
+	template<typename T>
+	struct _
+	{};
+
+	template<typename T, typename ...Args>
+	variant(_<T>, Args &&...args)
+		: _data(), _type_id(id_of<T>())
+	{
+		static_assert(_is_in_v<T, Ts...>);
+		this->no_check_emplace<T>(std::forward<Args>(args)...);
+	}
 
 public:
 	template<typename T>
 	static constexpr int id_of(void)
 	{
-		return _get_id<T, Ts...>::value;
+		static_assert(_is_in_v<T, Ts...>);
+		return _get_id_v<T, Ts...>;
 	}
 
 	template<typename T, typename ...Args>
 	static self_t make(Args &&...args)
 	{
 		static_assert(_is_in_v<T, Ts...>);
-		return variant<Ts...>(new T(std::forward<Args>(args)...), id_of<T>());
+		return self_t(_<T>{}, std::forward<Args>(args)...);
 	}
 
 	~variant(void)
 	{
-		if (this->_data == nullptr)
+		if (this->_type_id == -1)
 		{
 			return;
 		}
 
 		((this->_type_id == id_of<Ts>()
-		? (void)(delete static_cast<Ts *>(this->_data))
+		? (void)(this->no_check_get<Ts>().~Ts())
 		: (void)0 ), ...);
 	}
 
 	void clear(void)
 	{
-		if (this->_data == nullptr)
+		if (this->_type_id == -1)
 		{
 			return;
 		}
 
 		((this->_type_id == id_of<Ts>()
-		? (void)((delete static_cast<Ts *>(this->_data)), this->_data = nullptr, this->_type_id = -1)
+		? (void)((this->no_check_get<Ts>().~Ts()), this->_type_id = -1)
 		: (void)(0)
 		), ...);
 	}
 
 
 	variant(self_t const &other)
-		: _data(nullptr), _type_id(other._type_id)
+		: _data(), _type_id(other._type_id)
 	{
-		if (other._data == nullptr)
+		if (other._type_id == -1)
 		{
 			return;
 		}
 
 		((this->_type_id == id_of<Ts>()
-		? (void)(this->_data = new Ts(*static_cast<Ts *>(other._data)))
+		? (void)(this->no_check_emplace<Ts>(other.no_check_get<Ts>()))
 		: (void)0), ...);
 	}
 
 	variant(self_t &&other)
-		: _data(other._data), _type_id(other._type_id)
+		: _data(), _type_id(other._type_id)
 	{
-		other._data = nullptr;
+		((this->_type_id == id_of<Ts>()
+		? (void)(this->no_check_emplace<Ts>(std::move(other.no_check_get<Ts>())))
+		: (void)0), ...);
+
 		other._type_id = -1;
 	}
 
@@ -144,7 +196,7 @@ public:
 		this->_type_id = other._type_id;
 
 		((this->_type_id == id_of<Ts>()
-		? (void)(this->_data = new Ts(*static_cast<Ts *>(other._data)))
+		? (void)(this->no_check_emplace<Ts>(other.no_check_get<Ts>()))
 		: (void)0), ...);
 
 		return *this;
@@ -158,16 +210,41 @@ public:
 		}
 
 		this->clear();
-
 		this->_type_id = other._type_id;
-		this->_data = other._data;
 
-		other._data = nullptr;
+		((this->_type_id == id_of<Ts>()
+		? (void)(this->no_check_emplace<Ts>(std::move(other.no_check_get<Ts>())))
+		: (void)0), ...);
+
 		other._type_id = -1;
 
 		return *this;
 	}
 
+
+	template<typename T, typename ...Args>
+	void emplace(Args &&...args)
+	{
+		static_assert(_is_in_v<T, Ts...>);
+		this->clear();
+		this->no_check_emplace<T>(std::forward<Args>(args)...);
+	}
+
+	template<typename T, std::enable_if_t<!std::is_same_v<T, self_t>>>
+	self_t &operator = (T const &val)
+	{
+		static_assert(_is_in_v<T, Ts...>);
+		this->emplace(val);
+		return *this;
+	}
+
+	template<typename T, std::enable_if_t<!std::is_same_v<T, self_t>>>
+	self_t &operator = (T &&val)
+	{
+		static_assert(_is_in_v<T, Ts...>);
+		this->emplace(std::move(val));
+		return *this;
+	}
 
 	template<typename T>
 	T *get_if(void)
@@ -175,7 +252,7 @@ public:
 		static_assert(_is_in_v<T, Ts...>);
 		if (this->_type_id == id_of<T>())
 		{
-			return static_cast<T *>(this->_data);
+			return &this->no_check_get<T>();
 		}
 		else
 		{
@@ -189,7 +266,7 @@ public:
 		static_assert(_is_in_v<T, Ts...>);
 		if (this->_type_id == id_of<T>())
 		{
-			return static_cast<T *>(this->_data);
+			return &this->no_check_get<T>();
 		}
 		else
 		{
@@ -198,41 +275,33 @@ public:
 	}
 
 	template<typename T>
-	T *get(void)
+	T &get(void)
 	{
 		static_assert(_is_in_v<T, Ts...>);
-		if (this->_type_id == id_of<T>())
-		{
-			return static_cast<T *>(this->_data);
-		}
-		else
+		if (this->_type_id != id_of<T>())
 		{
 			assert(false, "Bad type in variant::get");
-			return nullptr;
 		}
+		return this->no_check_get<T>();
 	}
-
 
 	template<typename T>
-	const T *get(void) const
+	T const &get(void) const
 	{
 		static_assert(_is_in_v<T, Ts...>);
-		if (this->_type_id == id_of<T>())
-		{
-			return static_cast<T *>(this->_data);
-		}
-		else
+		if (this->_type_id != id_of<T>())
 		{
 			assert(false, "Bad type in variant::get");
-			return nullptr;
 		}
+		return this->no_check_get<T>();
 	}
+
 
 
 	friend std::ostream &operator << (std::ostream &os, self_t const &v)
 	{
 		((v._type_id == id_of<Ts>()
-		? (void)(os << *static_cast<Ts *>(v._data))
+		? (void)(os << *reinterpret_cast<const Ts *>(v._data))
 		: (void)0), ...);
 		return os;
 	}
