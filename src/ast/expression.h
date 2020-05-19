06ef989adbb525d51fa2af848cfebe3b9ec58d4a
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2020-05-19 20:31:05 +0200

    changed how expressions are represented

--- src/ast/expression.h
+++ src/ast/expression.h
@@ -5,82 +5,171 @@
 
 #include "node.h"
 #include "typespec.h"
+#include "constant_value.h"
 
 namespace ast
 {
 
+struct expression;
+
 #define declare_node_type(x) struct x; using x##_ptr = std::unique_ptr<x>
 
-declare_node_type(expr_unresolved);
 declare_node_type(expr_identifier);
 declare_node_type(expr_literal);
 declare_node_type(expr_tuple);
 declare_node_type(expr_unary_op);
 declare_node_type(expr_binary_op);
+declare_node_type(expr_subscript);
 declare_node_type(expr_function_call);
 declare_node_type(expr_cast);
 
 #undef declare_node_type
 
-struct expression : node<
-	expr_unresolved,
+using expr_t = node<
 	expr_identifier,
 	expr_literal,
 	expr_tuple,
 	expr_unary_op,
 	expr_binary_op,
+	expr_subscript,
 	expr_function_call,
 	expr_cast
+>;
+
+enum class expression_type_kind
+{
+	lvalue,
+	lvalue_reference,
+	rvalue,
+	rvalue_reference,
+	function_name,
+};
+
+struct unresolved_expression
+{};
+
+struct constant_expression
+{
+	expression_type_kind kind;
+	typespec             type;
+	constant_value       value;
+	expr_t               expr;
+};
+
+struct dynamic_expression
+{
+	expression_type_kind kind;
+	typespec             type;
+	expr_t               expr;
+};
+
+struct tuple_expression
+{
+	bz::vector<expression> elems;
+	~tuple_expression(void) noexcept = default;
+};
+
+struct expression : bz::variant<
+	unresolved_expression,
+	constant_expression,
+	dynamic_expression,
+	tuple_expression
 >
 {
-	using base_t = node<
-		expr_unresolved,
-		expr_identifier,
-		expr_literal,
-		expr_tuple,
-		expr_unary_op,
-		expr_binary_op,
-		expr_function_call,
-		expr_cast
+	using base_t = bz::variant<
+		unresolved_expression,
+		constant_expression,
+		dynamic_expression,
+		tuple_expression
 	>;
 
 	using base_t::get;
-	using base_t::kind;
 
-	enum expr_type_kind
-	{
-		lvalue,
-		lvalue_reference,
-		rvalue,
-		rvalue_reference,
-		function_name,
-	};
+	auto kind(void) const noexcept
+	{ return this->base_t::index(); }
 
-	struct expr_type_t
-	{
-		expr_type_kind type_kind = rvalue;
-		typespec       expr_type;
-	};
+	template<typename T>
+	bool is(void) const noexcept
+	{ return this->kind() == base_t::index_of<T>; }
 
-	expr_type_t      expr_type;
-	lex::token_range tokens;
+	lex::src_tokens src_tokens;
 
 	template<typename ...Ts>
-	expression(lex::token_range _tokens, Ts &&...ts)
-		: base_t   (std::forward<Ts>(ts)...),
-		  expr_type{},
-		  tokens   (_tokens)
+	expression(lex::src_tokens _src_tokens, Ts &&...ts)
+		: base_t(std::forward<Ts>(ts)...),
+		  src_tokens(_src_tokens)
 	{}
 
 	expression(void)
-		: base_t   (),
-		  expr_type{},
-		  tokens   { nullptr, nullptr }
+		: base_t(),
+		  src_tokens{}
 	{}
 
-	lex::token_pos get_tokens_begin(void) const;
-	lex::token_pos get_tokens_pivot(void) const;
-	lex::token_pos get_tokens_end(void) const;
+	lex::token_pos get_tokens_begin(void) const { return this->src_tokens.begin; }
+	lex::token_pos get_tokens_pivot(void) const { return this->src_tokens.pivot; }
+	lex::token_pos get_tokens_end(void) const   { return this->src_tokens.end; }
+
+	bool is_function(void) const noexcept
+	{
+		auto const const_expr = this->get_if<constant_expression>();
+		return const_expr
+			&& const_expr->kind == expression_type_kind::function_name;
+	}
+
+	bool is_overloaded_function(void) const noexcept
+	{
+		auto const const_expr = this->get_if<constant_expression>();
+		return const_expr
+			&& const_expr->kind == expression_type_kind::function_name
+			&& const_expr->type.is_null();
+	}
+
+	std::pair<typespec const &, expression_type_kind> get_expr_type_and_kind(void) const
+	{
+		switch (this->kind())
+		{
+		case ast::expression::index_of<ast::constant_expression>:
+		{
+			auto &const_expr = this->get<ast::constant_expression>();
+			return { const_expr.type, const_expr.kind };
+		}
+		case ast::expression::index_of<ast::dynamic_expression>:
+		{
+			auto &dyn_expr = this->get<ast::dynamic_expression>();
+			return { dyn_expr.type, dyn_expr.kind };
+		}
+
+		case ast::expression::index_of<ast::tuple_expression>:
+			bz_assert(false);
+		default:
+			return { ast::typespec(), static_cast<ast::expression_type_kind>(0) };
+		}
+	}
+
+	std::pair<typespec &, expression_type_kind> get_expr_type_and_kind(void)
+	{
+		switch (this->kind())
+		{
+		case ast::expression::index_of<ast::constant_expression>:
+		{
+			auto &const_expr = this->get<ast::constant_expression>();
+			return { const_expr.type, const_expr.kind };
+		}
+		case ast::expression::index_of<ast::dynamic_expression>:
+		{
+			auto &dyn_expr = this->get<ast::dynamic_expression>();
+			return { dyn_expr.type, dyn_expr.kind };
+		}
+
+		case ast::expression::index_of<ast::tuple_expression>:
+		default:
+		{
+			bz_assert(false);
+			auto t = ast::typespec();
+			return { t, static_cast<expression_type_kind>(0) };
+		}
+		}
+	}
 };
 
 
@@ -88,24 +177,6 @@ struct decl_variable;
 struct decl_function;
 struct function_body;
 
-
-struct expr_unresolved
-{
-	lex::token_range expr;
-
-	expr_unresolved(lex::token_range _expr)
-		: expr(_expr)
-	{}
-
-	lex::token_pos get_tokens_begin(void) const
-	{ return this->expr.begin; }
-
-	lex::token_pos get_tokens_pivot(void) const
-	{ return this->expr.begin; }
-
-	lex::token_pos get_tokens_end(void) const
-	{ return this->expr.end; }
-};
 
 struct expr_identifier
 {
@@ -136,39 +207,15 @@ struct expr_identifier
 
 struct expr_literal
 {
-	struct _bool_true {};
-	struct _bool_false {};
-	struct _null {};
+	lex::token_range tokens;
 
-	using value_t = bz::variant<
-		uint64_t, double, bz::u8string, bz::u8char,
-		_bool_true, _bool_false, _null
-	>;
-	enum : uint32_t
-	{
-		integer_number        = value_t::index_of<uint64_t>,
-		floating_point_number = value_t::index_of<double>,
-		string                = value_t::index_of<bz::u8string>,
-		character             = value_t::index_of<bz::u8char>,
-		bool_true             = value_t::index_of<_bool_true>,
-		bool_false            = value_t::index_of<_bool_false>,
-		null                  = value_t::index_of<_null>,
-	};
+	expr_literal(lex::token_range _tokens)
+		: tokens(_tokens)
+	{}
 
-	value_t        value;
-	lex::token_pos src_pos;
-	uint32_t       type_kind = 0;
-
-	expr_literal(lex::token_pos stream);
-
-	lex::token_pos get_tokens_begin(void) const
-	{ return this->src_pos; }
-
-	lex::token_pos get_tokens_pivot(void) const
-	{ return this->src_pos; }
-
-	lex::token_pos get_tokens_end(void) const
-	{ return this->src_pos + 1; }
+	expr_literal(lex::token_pos it)
+		: tokens{ it, it + 1 }
+	{}
 };
 
 struct expr_tuple
@@ -193,20 +240,18 @@ struct expr_unary_op
 {
 	lex::token_pos op;
 	expression     expr;
-	function_body *op_body;
 
 	expr_unary_op(
 		lex::token_pos _op,
 		expression     _expr
 	)
 		: op     (_op),
-		  expr   (std::move(_expr)),
-		  op_body(nullptr)
+		  expr   (std::move(_expr))
 	{}
 
-	lex::token_pos get_tokens_begin(void) const;
-	lex::token_pos get_tokens_pivot(void) const;
-	lex::token_pos get_tokens_end(void) const;
+	lex::token_pos get_tokens_begin(void) const { return this->op; }
+	lex::token_pos get_tokens_pivot(void) const { return this->op; }
+	lex::token_pos get_tokens_end(void) const   { return this->expr.get_tokens_end(); }
 };
 
 struct expr_binary_op
@@ -214,7 +259,6 @@ struct expr_binary_op
 	lex::token_pos op;
 	expression     lhs;
 	expression     rhs;
-	function_body *op_body;
 
 	expr_binary_op(
 		lex::token_pos _op,
@@ -223,36 +267,54 @@ struct expr_binary_op
 	)
 		: op     (_op),
 		  lhs    (std::move(_lhs)),
-		  rhs    (std::move(_rhs)),
-		  op_body(nullptr)
+		  rhs    (std::move(_rhs))
 	{}
 
-	lex::token_pos get_tokens_begin(void) const;
-	lex::token_pos get_tokens_pivot(void) const;
-	lex::token_pos get_tokens_end(void) const;
+	lex::token_pos get_tokens_begin(void) const { return this->lhs.get_tokens_begin(); }
+	lex::token_pos get_tokens_pivot(void) const { return this->op; }
+	lex::token_pos get_tokens_end(void) const   { return this->rhs.get_tokens_end(); }
+};
+
+struct expr_subscript
+{
+	lex::src_tokens        src_tokens;
+	expression             base;
+	bz::vector<expression> indicies;
+
+	expr_subscript(
+		lex::src_tokens        _src_tokens,
+		expression             _base,
+		bz::vector<expression> _indicies
+	)
+		: src_tokens(_src_tokens),
+		  base      (std::move(_base)),
+		  indicies  (std::move(_indicies))
+	{}
+
+	lex::token_pos get_tokens_begin(void) const { return this->src_tokens.begin; }
+	lex::token_pos get_tokens_pivot(void) const { return this->src_tokens.pivot; }
+	lex::token_pos get_tokens_end(void) const   { return this->src_tokens.end; }
 };
 
 struct expr_function_call
 {
-	lex::token_pos         op;
-	expression             called;
+	lex::src_tokens        src_tokens;
 	bz::vector<expression> params;
 	function_body         *func_body;
 
 	expr_function_call(
-		lex::token_pos         _op,
-		expression             _called,
-		bz::vector<expression> _params
+		lex::src_tokens        _src_tokens,
+		bz::vector<expression> _params,
+		function_body         *_func_body
 	)
-		: op       (_op),
-		  called   (std::move(_called)),
-		  params   (std::move(_params)),
-		  func_body(nullptr)
+		: src_tokens(_src_tokens),
+		  params    (std::move(_params)),
+		  func_body (_func_body)
 	{}
 
-	lex::token_pos get_tokens_begin() const;
-	lex::token_pos get_tokens_pivot() const;
-	lex::token_pos get_tokens_end() const;
+	lex::token_pos get_tokens_begin() const { return this->src_tokens.begin; }
+	lex::token_pos get_tokens_pivot() const { return this->src_tokens.pivot; }
+	lex::token_pos get_tokens_end() const   { return this->src_tokens.end; }
 };
 
 struct expr_cast
@@ -260,7 +322,6 @@ struct expr_cast
 	lex::token_pos as_pos;
 	expression     expr;
 	typespec       type;
-	function_body *func_body;
 
 	expr_cast(
 		lex::token_pos _as_pos,
@@ -278,42 +339,55 @@ struct expr_cast
 };
 
 
+inline expression make_unresolved_expression(lex::src_tokens tokens)
+{ return expression(tokens, unresolved_expression()); }
 
 template<typename ...Args>
-expression make_expression(Args &&...args)
-{ return expression(std::forward<Args>(args)...); }
+expression make_dynamic_expression(lex::src_tokens tokens, Args &&...args)
+{ return expression(tokens, dynamic_expression{ std::forward<Args>(args)... }); }
 
 template<typename ...Args>
-expression make_expr_unresolved(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_unresolved>(std::forward<Args>(args)...)); }
+expression make_constant_expression(lex::src_tokens tokens, Args &&...args)
+{ return expression(tokens, constant_expression{ std::forward<Args>(args)... }); }
 
 template<typename ...Args>
-expression make_expr_identifier(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_identifier>(std::forward<Args>(args)...)); }
+expression make_tuple_expression(lex::src_tokens tokens, Args &&...args)
+{ return expression(tokens, tuple_expression{ std::forward<Args>(args)... }); }
+
 
 template<typename ...Args>
-expression make_expr_literal(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_literal>(std::forward<Args>(args)...)); }
+expr_t make_expr_identifier(Args &&...args)
+{ return expr_t(std::make_unique<expr_identifier>(std::forward<Args>(args)...)); }
 
 template<typename ...Args>
-expression make_expr_tuple(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_tuple>(std::forward<Args>(args)...)); }
+expr_t make_expr_literal(Args &&...args)
+{ return expr_t(std::make_unique<expr_literal>(std::forward<Args>(args)...)); }
 
 template<typename ...Args>
-expression make_expr_unary_op(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_unary_op>(std::forward<Args>(args)...)); }
+expr_t make_expr_tuple(Args &&...args)
+{ return expr_t(std::make_unique<expr_tuple>(std::forward<Args>(args)...)); }
 
 template<typename ...Args>
-expression make_expr_binary_op(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_binary_op>(std::forward<Args>(args)...)); }
+expr_t make_expr_unary_op(Args &&...args)
+{ return expr_t(std::make_unique<expr_unary_op>(std::forward<Args>(args)...)); }
 
 template<typename ...Args>
-expression make_expr_function_call(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_function_call>(std::forward<Args>(args)...)); }
+expr_t make_expr_binary_op(Args &&...args)
+{ return expr_t(std::make_unique<expr_binary_op>(std::forward<Args>(args)...)); }
 
 template<typename ...Args>
-expression make_expr_cast(lex::token_range tokens, Args &&...args)
-{ return expression(tokens, std::make_unique<expr_cast>(std::forward<Args>(args)...)); }
+expr_t make_expr_subscript(Args &&...args)
+{ return expr_t(std::make_unique<expr_subscript>(std::forward<Args>(args)...)); }
+
+template<typename ...Args>
+expr_t make_expr_function_call(Args &&...args)
+{ return expr_t(std::make_unique<expr_function_call>(std::forward<Args>(args)...)); }
+
+template<typename ...Args>
+expr_t make_expr_cast(Args &&...args)
+{ return expr_t(std::make_unique<expr_cast>(std::forward<Args>(args)...)); }
+
+
 
 } // namespace ast
 
