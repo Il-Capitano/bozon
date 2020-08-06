7384c52936def3dd19e8685eb5d824b1fba1da7a
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2020-08-07 00:24:28 +0200

    added a trap to assertion failures

--- include/bz/core.h
+++ include/bz/core.h
@@ -11,11 +11,11 @@
 #define bz_likely
 #define bz_unlikely
 
-#define bz_assert(expr, ...)                                                              \
-(!!(expr)                                                                                 \
-? (void)0                                                                                 \
-: (void)(                                                                                 \
-	_bz_assert_begin(#expr, __FILE__, __LINE__), _bz_assert_message(__VA_ARGS__), exit(1) \
+#define bz_assert(expr, ...)                                                                                \
+(!!(expr)                                                                                                   \
+? (void)0                                                                                                   \
+: (void)(                                                                                                   \
+    _bz_assert_begin(#expr, __FILE__, __LINE__), _bz_assert_message(__VA_ARGS__), __builtin_trap(), exit(1) \
 ))
 
 inline void _bz_assert_begin(const char *expr, const char *file, int line)
