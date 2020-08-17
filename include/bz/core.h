cba3211c372ebbfe4eece6cd2fb80065a154928f
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2020-08-17 23:00:30 +0200

    added `bz_unreachable` to replace `bz_assert(false)`

--- include/bz/core.h
+++ include/bz/core.h
@@ -11,25 +11,37 @@
 #define bz_likely
 #define bz_unlikely
 
-#define bz_assert(expr, ...)                                                                                \
-(!!(expr)                                                                                                   \
-? (void)0                                                                                                   \
-: (void)(                                                                                                   \
-    _bz_assert_begin(#expr, __FILE__, __LINE__), _bz_assert_message(__VA_ARGS__), __builtin_trap(), exit(1) \
+#define bz_assert(expr, ...)                                                                         \
+(!!(expr)                                                                                            \
+? (void)0                                                                                            \
+: (void)(                                                                                            \
+    bz::_assert_begin(#expr, __FILE__, __LINE__), bz::_assert_message(__VA_ARGS__), __builtin_trap() \
 ))
 
-inline void _bz_assert_begin(const char *expr, const char *file, int line)
+#define bz_unreachable \
+(bz::_unreachable_message(__FILE__, __LINE__), __builtin_trap())
+
+bz_begin_namespace
+
+inline void _assert_begin(const char *expr, const char *file, int line)
 {
 	std::cerr << "assertion failed at " << file << ':' << line << "\n"
 		"    expression: " << expr << '\n';
 }
 
-inline void _bz_assert_message(char const *message)
+inline void _assert_message(char const *message)
 {
 	std::cerr << "    message: " << message << '\n';
 }
 
-inline void _bz_assert_message()
+inline void _assert_message()
 {}
+
+inline void _unreachable_message(const char *file, int line)
+{
+	std::cerr << "hit unreachable code at " << file << ':' << line << "\n";
+}
+
+bz_end_namespace
 
 #endif // _bz_core_h__
