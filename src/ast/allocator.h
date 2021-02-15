b00642787b350576d89a7398b440e8e3886fe689
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2021-02-15 22:58:15 +0100

    added an allocator type to be used by bz::vector

--- src/ast/allocator.h
+++ src/ast/allocator.h
@@ -45,6 +45,24 @@ ast_unique_ptr<T> make_ast_unique(Ts &&...ts)
 	return ast_unique_ptr<T>(arena_allocator::allocate_and_construct<T>(std::forward<Ts>(ts)...));
 }
 
+template<typename T>
+struct vector_arena_allocator
+{
+	using value_type = T;
+	using size_type = size_t;
+	using difference_type = std::ptrdiff_t;
+	using propagate_on_container_move_assignment = std::true_type;
+
+	T *allocate(size_type n)
+	{ return reinterpret_cast<T *>(arena_allocator::sized_allocate(n * sizeof (T))); }
+
+	void deallocate(T *p, size_type n)
+	{ arena_allocator::sized_free(p, n * sizeof (T)); }
+};
+
+template<typename T>
+using arena_vector = bz::vector<T, vector_arena_allocator<T>>;
+
 } // namespace ast
 
 #endif // AST_ALLOCATOR_H
