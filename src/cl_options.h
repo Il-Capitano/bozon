04566206bf042cf2935df66c695068f08a4a7132
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2021-06-26 00:25:17 +0200

    add warnings for switch expressions

--- src/cl_options.h
+++ src/cl_options.h
@@ -151,7 +151,9 @@ template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element(
 template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--warn bad-float-math")>           = &warnings[static_cast<size_t>(ctx::warning_kind::bad_float_math)];
 template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--warn binary-stdout")>            = &warnings[static_cast<size_t>(ctx::warning_kind::binary_stdout)];
 template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--warn is-comptime-always-true")>  = &warnings[static_cast<size_t>(ctx::warning_kind::is_comptime_always_true)];
-static_assert(static_cast<size_t>(ctx::warning_kind::_last) == 19);
+template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--warn non-exhaustive-switch")>    = &warnings[static_cast<size_t>(ctx::warning_kind::non_exhaustive_switch)];
+template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--warn unneeded-else")>            = &warnings[static_cast<size_t>(ctx::warning_kind::unneeded_else)];
+static_assert(static_cast<size_t>(ctx::warning_kind::_last) == 21);
 
 template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--opt instcombine")>            = &optimizations[static_cast<size_t>(bc::optimization_kind::instcombine)];
 template<> inline constexpr auto *ctcli::value_storage_ptr<ctcli::group_element("--opt mem2reg")>                = &optimizations[static_cast<size_t>(bc::optimization_kind::mem2reg)];
