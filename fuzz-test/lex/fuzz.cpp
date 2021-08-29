cbb029c61fe3fa54c1166094ef232c0cd2235229
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2021-08-29 14:12:05 +0200

    added rules to build lex fuzzer

--- fuzz-test/lex/fuzz.cpp
+++ fuzz-test/lex/fuzz.cpp
@@ -1,20 +1,27 @@
 #include "lex/lexer.h"
 #include "ctx/lex_context.h"
 #include "ctx/global_context.h"
-#include "ctx/src_manager.h"
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size)
 {
-	static ctx::src_manager manager{};
+	static ctx::global_context global_ctx;
 
 	auto const input = bz::u8string_view(data, data + size);
 	if (input.verify())
 	{
-		ctx::lex_context lex_ctx{manager._global_ctx};
+		ctx::lex_context lex_ctx(global_ctx);
 		lex::get_tokens(input, 0, lex_ctx);
 	}
-	manager._global_ctx.clear_errors_and_warnings();
+	global_ctx.clear_errors_and_warnings();
 	return 0;
 }
 
 #include "lex/lexer.cpp"
+#include "ctx/global_context.cpp"
+#include "ctx/comptime_executor.cpp"
+#include "ctx/lex_context.cpp"
+#include "ast/statement.cpp"
+#include "ast/expression.cpp"
+#include "ast/typespec.cpp"
+#include "ast/allocator.cpp"
+#include "global_data.cpp"
