797d2edeb606a0401c4aba6aaabb455c1691014b
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2022-01-02 15:20:19 +0100

    fix tests

--- test/consteval_test.cpp
+++ test/consteval_test.cpp
@@ -14,8 +14,7 @@ static bz::optional<bz::u8string> consteval_guaranteed_test(ctx::global_context 
 {
 	ctx::lex_context lex_ctx(global_ctx);
 	ctx::parse_context parse_ctx(global_ctx);
-	ctx::decl_set global_decls = ctx::get_default_decls();
-	parse_ctx.global_decls = &global_decls;
+	parse_ctx.current_global_scope = global_ctx._builtin_global_scope;
 
 #define x(str, kind, res_value)                                                 \
 do {                                                                            \
@@ -80,8 +79,7 @@ static bz::optional<bz::u8string> consteval_try_test(ctx::global_context &global
 {
 	ctx::lex_context lex_ctx(global_ctx);
 	ctx::parse_context parse_ctx(global_ctx);
-	ctx::decl_set global_decls = ctx::get_default_decls();
-	parse_ctx.global_decls = &global_decls;
+	parse_ctx.current_global_scope = global_ctx._builtin_global_scope;
 
 #define x(str, kind, res_value)                                                 \
 do {                                                                            \
@@ -128,28 +126,6 @@ do {                                                                            
 	x("{ 0; __builtin_exp_f32(1.5f32) }", ast::constant_value::float32, std::exp(1.5f));
 	x("__builtin_sinh_f32(1.6f32)",        ast::constant_value::float32, std::sinh(1.6f));
 	x("{ 0; __builtin_sinh_f32(1.6f32) }", ast::constant_value::float32, std::sinh(1.6f));
-	x(R"({
-		function factorial(n) -> typeof n
-		{
-			if (n <= 1 as typeof n)
-			{
-				return n;
-			}
-			return n * factorial(n - 1 as typeof n);
-		}
-		factorial(10)
-	})", ast::constant_value::sint, 3628800);
-	x(R"({
-		function factorial(n) -> typeof n
-		{
-			if (n <= 1 as typeof n)
-			{
-				return n;
-			}
-			return n * factorial(n - 1 as typeof n);
-		}
-		factorial(10u)
-	})", ast::constant_value::uint, 3628800);
 	x(R"({
 		function factorial(n) -> typeof n
 		{
