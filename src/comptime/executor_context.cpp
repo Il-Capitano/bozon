e9931fb14759e08207fa0ed5de37eb9932dab247
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2022-11-16 18:16:35 +0100

    initial idea of how the `executor_context` should look like

---
+++ src/comptime/executor_context.cpp
@@ -0,0 +1,46 @@
+#include "executor_context.h"
+
+namespace comptime
+{
+
+uint8_t *executor_context::get_memory(ptr_t ptr, size_t size)
+{
+	bz_unreachable;
+}
+
+ptr_t executor_context::do_alloca(size_t size, size_t align)
+{
+	bz_unreachable;
+}
+
+void executor_context::set_current_instruction_value(instruction_value value)
+{
+	this->current_function->instruction_values[this->instruction_value_offset] = value;
+}
+
+instruction_value executor_context::get_instruction_value(instructions::arg_t inst_index)
+{
+	return this->current_function->instruction_values[inst_index];
+}
+
+void executor_context::do_jump(basic_block const *next_bb)
+{
+	this->next_bb = next_bb;
+}
+
+void executor_context::advance(void)
+{
+	if (this->next_bb != nullptr)
+	{
+		bz_assert(this->next_bb >= this->current_function->blocks.data() && this->next_bb < this->current_function->blocks.data_end());
+		this->current_instruction = this->next_bb->instructions.data();
+		this->instruction_value_offset = this->next_bb->instruction_value_offset;
+	}
+	else
+	{
+		bz_assert(!this->current_instruction->is_terminator());
+		++this->current_instruction;
+	}
+}
+
+} // namespace comptime
---
+++ src/comptime/executor_context.h
@@ -0,0 +1,31 @@
+#ifndef COMPTIME_EXECUTOR_CONTEXT_H
+#define COMPTIME_EXECUTOR_CONTEXT_H
+
+#include "instructions.h"
+
+namespace comptime
+{
+
+struct executor_context
+{
+	instruction const *current_instruction;
+	function *current_function;
+	ptr_t function_stack_base;
+
+	size_t instruction_value_offset;
+	basic_block const *next_bb;
+
+	uint8_t *get_memory(ptr_t ptr, size_t size);
+	ptr_t do_alloca(size_t size, size_t align);
+
+	void set_current_instruction_value(instruction_value value);
+	instruction_value get_instruction_value(instructions::arg_t inst_index);
+
+	void do_jump(basic_block const *next_bb);
+
+	void advance(void);
+};
+
+} // namespace comptime
+
+#endif // COMPTIME_EXECUTOR_CONTEXT_H
