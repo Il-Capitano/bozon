a767a393fe2b46fe83046c5b6629769c51840126
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2022-12-14 20:57:48 +0100

    move some code into a header

--- src/comptime/instructions.h
+++ src/comptime/instructions.h
@@ -3,36 +3,11 @@
 
 #include "core.h"
 #include "types.h"
+#include "values.h"
 #include "ctx/warnings.h"
 
 namespace comptime
 {
-
-enum class value_type
-{
-	i1, i8, i16, i32, i64,
-	f32, f64,
-	ptr,
-	none, any,
-};
-
-struct none_t
-{};
-
-using ptr_t = uint64_t;
-
-union instruction_value
-{
-	bool i1;
-	uint8_t i8;
-	uint16_t i16;
-	uint32_t i32;
-	uint64_t i64;
-	float32_t f32;
-	float64_t f64;
-	ptr_t ptr;
-	none_t none;
-};
 
 struct instruction;
 
