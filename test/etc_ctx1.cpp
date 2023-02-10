ce60c10cc03227914f497383277a82388a9776f9
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2023-02-10 15:27:50 +0100

    separate `test/etc_ctx.cpp` into two files to make GCC happy in the CI build

--- test/etc_ctx1.cpp
+++ test/etc_ctx1.cpp
@@ -4,7 +4,3 @@
 #include "ctx/global_context.cpp"
 #include "ctx/lex_context.cpp"
 #include "ctx/parse_context.cpp"
-#include "ctx/bitcode_context.cpp"
-#include "ctx/builtin_operators.cpp"
-#include "ctx/error.cpp"
-#include "ctx/warnings.cpp"
---
+++ test/etc_ctx2.cpp
@@ -0,0 +1,7 @@
+// cpp files that are needed for compilation,
+// but have no tests yet are included here
+
+#include "ctx/bitcode_context.cpp"
+#include "ctx/builtin_operators.cpp"
+#include "ctx/error.cpp"
+#include "ctx/warnings.cpp"
