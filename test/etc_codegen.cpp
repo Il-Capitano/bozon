88adb9c91eea6b4452cd6c6ab9a79f2b7981d3dd
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2023-07-06 21:53:38 +0200

    fix test compilation

---
+++ test/etc_codegen.cpp
@@ -0,0 +1,6 @@
+// cpp files that are needed for compilation,
+// but have no tests yet are included here
+
+#include "codegen/llvm_latest/backend_context.cpp"
+#include "codegen/llvm_latest/bitcode_context.cpp"
+#include "codegen/llvm_latest/emit_bitcode.cpp"
