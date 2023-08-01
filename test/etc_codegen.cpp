f4c49c623fed5a35ecc722b17366ca494448a851
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2023-08-01 15:13:06 +0200

    don't enable any backends for tests

--- test/etc_codegen.cpp
+++ test/etc_codegen.cpp
@@ -1,12 +1,5 @@
 // cpp files that are needed for compilation,
 // but have no tests yet are included here
 
-#include "codegen/llvm_latest/abi/generic.cpp"
-#include "codegen/llvm_latest/abi/microsoft_x64.cpp"
-#include "codegen/llvm_latest/abi/systemv_amd64.cpp"
-#include "codegen/llvm_latest/backend_context.cpp"
-#include "codegen/llvm_latest/bitcode_context.cpp"
-#include "codegen/llvm_latest/emit_bitcode.cpp"
-#include "codegen/llvm_latest/target_properties.cpp"
 #include "codegen/target.cpp"
 #include "codegen/backend_context.cpp"
