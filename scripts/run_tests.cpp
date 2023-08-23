31fc938f8616cbb8d5a29f0764597a2691928d97
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2023-08-23 14:22:21 +0200

    add behavior tests

--- scripts/run_tests.cpp
+++ scripts/run_tests.cpp
@@ -13,25 +13,13 @@ namespace fs = std::filesystem;
 
 #ifdef _WIN32
 constexpr bz::u8string_view bozon_default = "bin/windows-debug/bozon.exe";
+constexpr bz::u8string_view clang_default = "clang";
+constexpr bz::u8string_view os_exe_extension = ".exe";
 #else
 constexpr bz::u8string_view bozon_default = "bin/linux-debug/bozon";
+constexpr bz::u8string_view clang_default = "clang-16";
+constexpr bz::u8string_view os_exe_extension = ".out";
 #endif
-
-struct promise_holder_base
-{
-	virtual ~promise_holder_base(void) = default;
-};
-
-template<typename T>
-struct promise_holder : promise_holder_base
-{
-	promise_holder(std::promise<T> p)
-		: _p(std::move(p))
-	{}
-
-private:
-	std::promise<T> _p;
-};
 
 struct thread_pool
 {
@@ -164,7 +152,7 @@ static void remove_ansi_escape_sequences(bz::u8string &s)
 
 struct test_fail_info_t
 {
-	bz::u8string command;
+	bz::vector<bz::u8string> commands;
 	bz::u8string test_file;
 	bz::process_result_t process_result;
 	bz::vector<bz::u8string> wanted_diagnostics;
@@ -178,7 +166,10 @@ struct test_run_result_t
 
 static void print_test_fail_info(test_fail_info_t const &fail_info)
 {
-	bz::print("{}\n", fail_info.command);
+	for (auto const &command : fail_info.commands)
+	{
+		bz::print("{}\n", command);
+	}
 	if (fail_info.process_result.stdout_string != "")
 	{
 		bz::print("stdout:\n{}\n", fail_info.process_result.stdout_string);
@@ -255,6 +246,34 @@ static bz::vector<bz::u8string> get_diagnostics_from_output(bz::u8string_view ou
 	return result;
 }
 
+static bz::u8string get_behavior_output_from_file(bz::u8string_view filename)
+{
+	auto const filename_std_string = std::string(filename.data(), filename.size());
+	auto file = std::ifstream(filename_std_string);
+
+	bz::u8string result;
+	std::string line;
+	while (std::getline(file, line))
+	{
+		auto line_sv = bz::u8string_view(line.data(), line.data() + line.size());
+		if (line_sv.ends_with('\r'))
+		{
+			line_sv = line_sv.substring(0, line_sv.length() - 1);
+		}
+		if (line_sv.starts_with("// "))
+		{
+			result += line_sv.substring(3);
+			result += '\n';
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return result;
+}
+
 static bz::vector<bz::u8string> get_diagnostics_from_file(bz::u8string_view filename)
 {
 	auto const filename_std_string = std::string(filename.data(), filename.size());
@@ -294,6 +313,85 @@ static bz::vector<bz::u8string> get_diagnostics_from_file(bz::u8string_view file
 	return result;
 }
 
+static bz::optional<test_fail_info_t> run_behavior_test_file(
+	bz::u8string_view bozon,
+	bz::vector<bz::u8string> flags,
+	bz::u8string_view file,
+	bz::u8string_view out_file,
+	bz::u8string_view clang,
+	bz::u8string_view out_exe
+)
+{
+	flags.push_back(file);
+	auto const flags_size = flags.size();
+	for (bz::u8string_view const emit_kind : { "obj", "c" })
+	{
+		// compile
+		flags.push_back(bz::format("--emit={}", emit_kind));
+		bz::u8string out_file_with_extension = out_file;
+		if (emit_kind == "obj")
+		{
+			out_file_with_extension += ".o";
+		}
+		else if (emit_kind == "c")
+		{
+			out_file_with_extension += ".c";
+		}
+		flags.push_back("-o");
+		flags.push_back(out_file_with_extension);
+		auto compilation_result = bz::run_process(bozon, flags);
+		remove_ansi_escape_sequences(compilation_result.stdout_string);
+		remove_ansi_escape_sequences(compilation_result.stderr_string);
+		if (compilation_result.return_code != 0 || compilation_result.stdout_string != "" || compilation_result.stderr_string != "")
+		{
+			return test_fail_info_t{
+				.commands = { bz::make_command_string(bozon, flags) },
+				.test_file = file,
+				.process_result = std::move(compilation_result),
+				.wanted_diagnostics = {},
+			};
+		}
+
+		auto const link_args = bz::array<bz::u8string_view, 3>{
+			out_file_with_extension,
+			"-o",
+			out_exe
+		};
+		auto link_result = bz::run_process(clang, link_args);
+		if (link_result.return_code != 0)
+		{
+			return test_fail_info_t{
+				.commands = { bz::make_command_string(bozon, flags), bz::make_command_string(clang, link_args) },
+				.test_file = file,
+				.process_result = std::move(link_result),
+				.wanted_diagnostics = {},
+			};
+		}
+
+		auto run_result = bz::run_process(out_exe, bz::array_view<bz::u8string_view const>{});
+		run_result.stdout_string.erase('\r');
+		auto const expected_output = get_behavior_output_from_file(file);
+		if (run_result.return_code != 0 || run_result.stderr_string != "" || run_result.stdout_string != expected_output)
+		{
+			return test_fail_info_t{
+				.commands = {
+					bz::make_command_string(bozon, flags),
+					bz::make_command_string(clang, link_args),
+					bz::make_command_string(out_exe, bz::array_view<bz::u8string_view const>{})
+				},
+				.test_file = file,
+				.process_result = std::move(run_result),
+				.wanted_diagnostics = {},
+			};
+		}
+
+		fs::remove(std::string_view(out_file_with_extension.data_as_char_ptr(), out_file_with_extension.size()));
+		fs::remove(std::string_view(out_exe.data(), out_exe.size()));
+		flags.resize(flags_size);
+	}
+	return {};
+}
+
 static bz::optional<test_fail_info_t> run_success_test_file(bz::u8string_view bozon, bz::vector<bz::u8string> flags, bz::u8string_view file)
 {
 	flags.push_back(file);
@@ -306,7 +404,7 @@ static bz::optional<test_fail_info_t> run_success_test_file(bz::u8string_view bo
 		if (compilation_result.return_code != 0 || compilation_result.stdout_string != "" || compilation_result.stderr_string != "")
 		{
 			return test_fail_info_t{
-				.command = bz::make_command_string(bozon, flags),
+				.commands = { bz::make_command_string(bozon, flags) },
 				.test_file = file,
 				.process_result = std::move(compilation_result),
 				.wanted_diagnostics = {},
@@ -335,7 +433,7 @@ static bz::optional<test_fail_info_t> run_warning_test_file(bz::u8string_view bo
 		)
 		{
 			return test_fail_info_t{
-				.command = bz::make_command_string(bozon, flags),
+				.commands = { bz::make_command_string(bozon, flags) },
 				.test_file = file,
 				.process_result = std::move(compilation_result),
 				.wanted_diagnostics = std::move(wanted_diagnostics),
@@ -372,7 +470,7 @@ static bz::optional<test_fail_info_t> run_error_test_file(bz::u8string_view bozo
 	)
 	{
 		return test_fail_info_t{
-			.command = bz::make_command_string(bozon, flags),
+			.commands = { bz::make_command_string(bozon, flags) },
 			.test_file = file,
 			.process_result = std::move(first_result),
 			.wanted_diagnostics = std::move(wanted_diagnostics),
@@ -380,6 +478,83 @@ static bz::optional<test_fail_info_t> run_error_test_file(bz::u8string_view bozo
 	}
 
 	return {};
+}
+
+static test_run_result_t run_behavior_tests(bz::u8string_view bozon, bz::vector<bz::u8string> common_flags, bz::u8string_view clang, thread_pool &pool)
+{
+	auto const files = get_files_in_folder("tests/behavior");
+	if (files.empty())
+	{
+		return { .passed_count = 0, .total_count = 0 };
+	}
+	auto const max_filename_length = files.transform([](auto const &file) { return file.generic_string().length() + 3; }).max(60);
+
+	auto const tests_temp_folder = fs::path("tests/temp");
+	fs::create_directories(tests_temp_folder);
+
+	bz::vector<test_fail_info_t> test_fail_infos;
+
+	bz::print("running tests in tests/success:\n");
+
+	auto futures = files.transform([&](auto const &file) {
+		auto file_string = bz::u8string(file.generic_string().c_str());
+		auto out_file = tests_temp_folder / file.filename();
+		auto out_file_string = bz::u8string(out_file.generic_string().c_str());
+		out_file += std::string_view(os_exe_extension.data(), os_exe_extension.size());
+		auto out_exe_string = bz::u8string(out_file.generic_string().c_str());
+		return pool.push_task([
+			bozon,
+			clang,
+			common_flags = common_flags.as_array_view(),
+			file_string = std::move(file_string),
+			out_file_string = std::move(out_file_string),
+			out_exe_string = std::move(out_exe_string)
+		]() {
+			return run_behavior_test_file(bozon, common_flags, file_string, out_file_string, clang, out_exe_string);
+		});
+	}).collect();
+	bz_assert(futures.size() == files.size());
+
+	size_t passed_count = 0;
+	for (auto const i : bz::iota(0, files.size()))
+	{
+		auto const &file = files[i];
+		auto &run_result_future = futures[i];
+		auto const file_string_ = file.generic_string();
+		auto const file_string = bz::u8string_view(file_string_.data(), file_string_.data() + file_string_.size());
+		bz::print("    {:.<{}}", max_filename_length, file_string);
+		fflush(stdout);
+
+		auto run_result = run_result_future.get();
+
+		if (run_result.has_value())
+		{
+			bz::print("{}FAIL{}\n", colors::bright_red, colors::clear);
+			print_test_fail_info(run_result.get());
+			test_fail_infos.push_back(std::move(run_result.get()));
+		}
+		else
+		{
+			bz::print("{}OK{}\n", colors::bright_green, colors::clear);
+			passed_count += 1;
+		}
+	}
+
+	auto const passed_percentage = static_cast<double>(100 * passed_count) / static_cast<double>(files.size());
+	auto const color = passed_count == files.size() ? colors::bright_green : colors::bright_red;
+	bz::print(
+		"{}{}/{}{} ({}{:.2f}%{}) tests passed\n",
+		color, passed_count, files.size(), colors::clear,
+		color, passed_percentage, colors::clear
+	);
+
+	for (auto const &info : test_fail_infos)
+	{
+		bz::print("\n{}FAILED:{} {}:\n", colors::bright_red, colors::clear, info.test_file);
+		print_test_fail_info(info);
+	}
+
+	return { passed_count, files.size() };
 }
 
 static test_run_result_t run_success_tests(bz::u8string_view bozon, bz::vector<bz::u8string> common_flags, thread_pool &pool)
@@ -573,16 +748,25 @@ int main(int argc, char const * const *argv)
 		.collect();
 
 	bz::u8string_view bozon = "";
+	bz::u8string_view clang = "";
 	for (auto const arg : args)
 	{
 		if (arg.starts_with("--bozon="))
 		{
 			bozon = arg.substring(bz::u8string_view("--bozon=").length());
 		}
+		else if (arg.starts_with("--clang="))
+		{
+			clang = arg.substring(bz::u8string_view("--clang=").length());
+		}
 	}
 	if (bozon == "")
 	{
 		bozon = bozon_default;
+	}
+	if (clang == "")
+	{
+		clang = clang_default;
 	}
 
 	auto const common_flags = bz::vector<bz::u8string>{
@@ -594,6 +778,7 @@ int main(int argc, char const * const *argv)
 
 	auto pool = thread_pool(std::thread::hardware_concurrency());
 
+	auto const behavior_result = run_behavior_tests(bozon, common_flags, clang, pool);
 	auto const success_result = run_success_tests(bozon, common_flags, pool);
 	auto const warning_result = run_warning_tests(bozon, common_flags, pool);
 	auto const error_result = run_error_tests(bozon, common_flags, pool);
@@ -611,12 +796,19 @@ int main(int argc, char const * const *argv)
 
 	bz::print("\nsummary:\n");
 
+	print_section_info(behavior_result, "tests/behavior");
 	print_section_info(success_result, "tests/success");
 	print_section_info(warning_result, "tests/warning");
 	print_section_info(error_result, "tests/error");
 
-	auto const passed_count = success_result.passed_count + warning_result.passed_count + error_result.passed_count;
-	auto const total_count = success_result.total_count + warning_result.total_count + error_result.total_count;
+	auto const passed_count = behavior_result.passed_count
+		+ success_result.passed_count
+		+ warning_result.passed_count
+		+ error_result.passed_count;
+	auto const total_count = behavior_result.total_count
+		+ success_result.total_count
+		+ warning_result.total_count
+		+ error_result.total_count;
 
 	auto const passed_percentage = static_cast<double>(100 * passed_count) / static_cast<double>(total_count);
 	auto const color = passed_count == total_count ? colors::bright_green : colors::bright_red;
