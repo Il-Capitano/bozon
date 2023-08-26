130f6fbd4dc13e460a0fd4b74e9031ed21d3e736
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2023-08-26 20:09:52 +0200

    update config and CI for LLVM 17

--- scripts/generate_config.cpp
+++ scripts/generate_config.cpp
@@ -84,22 +84,14 @@ static bz::u8string to_upper(bz::u8string_view s)
 static bool has_llvm(void)
 {
 	return bz::run_process("llvm-config", bz::array_view<char const * const>{ "--version" }).return_code != 0
-		|| bz::run_process("llvm-config-16", bz::array_view<char const * const>{ "--version" }).return_code != 0;
+		|| bz::run_process("llvm-config-17", bz::array_view<char const * const>{ "--version" }).return_code != 0;
 }
 
 static bz::optional<bz::u8string> get_llvm_default_target(void)
 {
+	for (auto const llvm_config : { "llvm-config", "llvm-config-17" })
 	{
-		auto process_result = bz::run_process("llvm-config", bz::array_view<char const * const>{ "--host-target" });
-		if (process_result.return_code == 0)
-		{
-			trim(process_result.stdout_string);
-			return std::move(process_result.stdout_string);
-		}
-	}
-
-	{
-		auto process_result = bz::run_process("llvm-config-16", bz::array_view<char const * const>{ "--host-target" });
+		auto process_result = bz::run_process(llvm_config, bz::array_view<char const * const>{ "--host-target" });
 		if (process_result.return_code == 0)
 		{
 			trim(process_result.stdout_string);
