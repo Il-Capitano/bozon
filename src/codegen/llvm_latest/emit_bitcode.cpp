cffb6f7fbf990f0effc99168c19bdd071f6ec4a9
Il-Capitano <52455591+Il-Capitano@users.noreply.github.com>
2023-09-01 02:15:39 +0200

    check for signed division overflow

--- src/codegen/llvm_latest/emit_bitcode.cpp
+++ src/codegen/llvm_latest/emit_bitcode.cpp
@@ -1589,9 +1589,55 @@ static val_ptr emit_builtin_binary_divide(
 	auto const lhs_val = emit_bitcode(lhs, context, nullptr).get_value(context.builder);
 	auto const rhs_val = emit_bitcode(rhs, context, nullptr).get_value(context.builder);
 
-	auto const result_val = ast::is_signed_integer_kind(lhs_kind) ? context.builder.CreateSDiv(lhs_val, rhs_val, "div_tmp")
-		: ast::is_unsigned_integer_kind(lhs_kind) ? context.builder.CreateUDiv(lhs_val, rhs_val, "div_tmp")
-		: context.builder.CreateFDiv(lhs_val, rhs_val, "div_tmp");
+	auto const result_val = [&]() -> llvm::Value * {
+		if (ast::is_signed_integer_kind(lhs_kind))
+		{
+			auto const result_type = lhs_val->getType();
+			auto const min_value = [&]() {
+				switch (lhs_kind)
+				{
+				case ast::type_info::int8_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int8_t>::min());
+				case ast::type_info::int16_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int16_t>::min());
+				case ast::type_info::int32_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int32_t>::min());
+				case ast::type_info::int64_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int64_t>::min());
+				default:
+					bz_unreachable;
+				}
+			}();
+			auto const lhs_is_overflow = context.builder.CreateICmpEQ(lhs_val, min_value);
+			auto const rhs_is_overflow = context.builder.CreateICmpEQ(rhs_val, llvm::ConstantInt::getSigned(result_type, -1));
+			auto const is_overflow = context.builder.CreateAnd(lhs_is_overflow, rhs_is_overflow);
+
+			auto const begin_bb = context.builder.GetInsertBlock();
+			auto const non_overflow_bb = context.add_basic_block("div_overflow_check");
+			auto const end_bb = context.add_basic_block("div_overflow_check_end");
+
+			context.builder.CreateCondBr(is_overflow, end_bb, non_overflow_bb);
+			context.builder.SetInsertPoint(non_overflow_bb);
+			auto const non_overflow_result = context.builder.CreateSDiv(lhs_val, rhs_val, "div_tmp");
+			context.builder.CreateBr(end_bb);
+
+			context.builder.SetInsertPoint(end_bb);
+			auto const result = context.builder.CreatePHI(result_type, 2, "div_tmp");
+			result->addIncoming(non_overflow_result, non_overflow_bb);
+			result->addIncoming(min_value, begin_bb);
+
+			return result;
+		}
+		else if (ast::is_unsigned_integer_kind(lhs_kind))
+		{
+			return context.builder.CreateUDiv(lhs_val, rhs_val, "div_tmp");
+		}
+		else
+		{
+			return context.builder.CreateFDiv(lhs_val, rhs_val, "div_tmp");
+		}
+	}();
+
 	if (result_address == nullptr)
 	{
 		return val_ptr::get_value(result_val);
@@ -1623,9 +1669,55 @@ static val_ptr emit_builtin_binary_divide_eq(
 	bz_assert(lhs_val_ref.kind == val_ptr::reference);
 	auto const lhs_val = lhs_val_ref.get_value(context.builder);
 
-	auto const res = ast::is_signed_integer_kind(lhs_kind) ? context.builder.CreateSDiv(lhs_val, rhs_val, "div_tmp")
-		: ast::is_unsigned_integer_kind(lhs_kind) ? context.builder.CreateUDiv(lhs_val, rhs_val, "div_tmp")
-		: context.builder.CreateFDiv(lhs_val, rhs_val, "div_tmp");
+	auto const res = [&]() -> llvm::Value * {
+		if (ast::is_signed_integer_kind(lhs_kind))
+		{
+			auto const result_type = lhs_val->getType();
+			auto const min_value = [&]() {
+				switch (lhs_kind)
+				{
+				case ast::type_info::int8_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int8_t>::min());
+				case ast::type_info::int16_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int16_t>::min());
+				case ast::type_info::int32_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int32_t>::min());
+				case ast::type_info::int64_:
+					return llvm::ConstantInt::getSigned(result_type, std::numeric_limits<int64_t>::min());
+				default:
+					bz_unreachable;
+				}
+			}();
+			auto const lhs_is_overflow = context.builder.CreateICmpEQ(lhs_val, min_value);
+			auto const rhs_is_overflow = context.builder.CreateICmpEQ(rhs_val, llvm::ConstantInt::getSigned(result_type, -1));
+			auto const is_overflow = context.builder.CreateAnd(lhs_is_overflow, rhs_is_overflow);
+
+			auto const begin_bb = context.builder.GetInsertBlock();
+			auto const non_overflow_bb = context.add_basic_block("div_overflow_check");
+			auto const end_bb = context.add_basic_block("div_overflow_check_end");
+
+			context.builder.CreateCondBr(is_overflow, end_bb, non_overflow_bb);
+			context.builder.SetInsertPoint(non_overflow_bb);
+			auto const non_overflow_result = context.builder.CreateSDiv(lhs_val, rhs_val, "div_tmp");
+			context.builder.CreateBr(end_bb);
+
+			context.builder.SetInsertPoint(end_bb);
+			auto const result = context.builder.CreatePHI(result_type, 2, "div_tmp");
+			result->addIncoming(non_overflow_result, non_overflow_bb);
+			result->addIncoming(min_value, begin_bb);
+
+			return result;
+		}
+		else if (ast::is_unsigned_integer_kind(lhs_kind))
+		{
+			return context.builder.CreateUDiv(lhs_val, rhs_val, "div_tmp");
+		}
+		else
+		{
+			return context.builder.CreateFDiv(lhs_val, rhs_val, "div_tmp");
+		}
+	}();
+
 	context.builder.CreateStore(res, lhs_val_ref.val);
 	if (result_address == nullptr)
 	{
